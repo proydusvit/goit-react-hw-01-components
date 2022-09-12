59e70da4e422f3c48f72c290964015e6cdfa6f45
Vlad Shestavetskyi <vladislavshsh@gmail.com>
2022-09-12 20:49:26 +0300

    f

--- src/components/statistics/statistics.jsx
+++ src/components/statistics/statistics.jsx
@@ -6,7 +6,7 @@ const Statistics = (
 ) => {
     return (
         <section className={styles.statistics}>
-            <h2 className={styles.title}>{title}</h2>
+            {title && <h2 className={styles.title}>{title}</h2>}
 
             <ul className={styles.stat_list}>
                 {stats.map(stat => (
@@ -24,10 +24,10 @@ Statistics.propTypes = {
     title: PropTypes.string.isRequired,
     stats: PropTypes.arrayOf(
         PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            label: PropTypes.string.isRequired,
-            percentage: PropTypes.number.isRequired,
+            id: PropTypes.string,
+            label: PropTypes.string,
+            percentage: PropTypes.number,
         })
-    )
+    ).isRequired
 }
 export default Statistics;
